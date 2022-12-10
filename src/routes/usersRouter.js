87eaf31c287dd855ed60637658062b153748ee3d
mick84 <demenkoff1984@yahoo.com>
2022-12-11 01:26:44 +0200

    corrected

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -7,8 +7,8 @@ usersRouter.get("/", (req, res) => {
   try {
     const users = utils.getUsers();
     res.status(200).json(users);
-  } catch ({ message }) {
-    res.status(404).send(message);
+  } catch (error) {
+    res.status(404).json(error);
   }
 });
 usersRouter.post("/", (req, res) => {
@@ -20,15 +20,15 @@ usersRouter.post("/", (req, res) => {
     }
     const response = utils.addUser(body);
     res.status(201).json(response);
-  } catch ({ message }) {
-    res.status(409).send(message);
+  } catch (error) {
+    res.status(409).json(error);
   }
 });
 usersRouter.get("/:id", (req, res) => {
   try {
     res.status(200).json(utils.getUser(req.params.id));
-  } catch ({ message }) {
-    res.status(404).send(message);
+  } catch (error) {
+    res.status(404).json(error);
   }
 });
 
@@ -37,16 +37,16 @@ usersRouter.patch("/:id", (req, res) => {
     const { id } = req.params;
     const response = utils.updateUser(id, req.body);
     res.status(200).json(response);
-  } catch ({ message }) {
-    res.status(404).send(message);
+  } catch (error) {
+    res.status(404).json(error);
   }
 });
 usersRouter.delete("/:id", (req, res) => {
   try {
     const response = utils.deleteUser(req.params.id);
     res.status(200).json(response);
-  } catch ({ message }) {
-    res.status(404).send(message);
+  } catch (error) {
+    res.status(404).json(error);
   }
 });
 usersRouter.get("/:id/accounts", (req, res) => {
@@ -54,8 +54,8 @@ usersRouter.get("/:id/accounts", (req, res) => {
     const user = utils.getUser(req.params.id);
     const accounts = utils.getUserAccounts(user.passportID);
     res.status(200).json(accounts);
-  } catch ({ message }) {
-    res.status(404).send(message);
+  } catch (error) {
+    res.status(404).json(error);
   }
 });
 
