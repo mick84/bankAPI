de4b062229b6226991751ae09dd2cb726b669de7
mick84 <demenkoff1984@yahoo.com>
2022-12-19 15:06:20 +0200

    last

--- src/helpers.js
+++
@@ -1,9 +0,0 @@
-import * as url from "url";
-export const __filename = url.fileURLToPath(import.meta.url);
-export const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-//console.log(__dirname, __filename);
-export const validateBody = (body, model) => {
-  if (Object.keys(body).some((key) => !(key in model.schema.paths))) {
-    throw new Error("Extra data provided!");
-  }
-};
